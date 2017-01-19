5a6cf41c71ee9ecbb0e04f1a684601fc0c9f013c
malens <malens@bitbucket.org>
2017-01-19 12:49:20 +0100

    added combobox to choose # of divisions

--- src/lshape/plot.java
+++ src/lshape/plot.java
@@ -3,6 +3,8 @@ import net.ericaro.surfaceplotter.JSurfacePanel;
 import net.ericaro.surfaceplotter.surface.ArraySurfaceModel;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import static javafx.application.Application.launch;
 
@@ -14,7 +16,9 @@ public class plot {
     private JFrame window = new JFrame();
     private JSurfacePanel plotFrame = new JSurfacePanel();
     private JPanel panel = new JPanel();
-
+    private int n = 8;
+    private Integer[] possibleDivisions;
+    private JComboBox<Integer> divisionList;
 
 
 
@@ -26,13 +30,39 @@ public class plot {
         this.plotFrame.setConfigurationVisible(false);
         this.window.getContentPane().add(plotFrame, BorderLayout.CENTER);
         this.window.getContentPane().add(panel, BorderLayout.SOUTH);
+
+        this.possibleDivisions = new Integer[15];
+        for (int i = 1; i<=15;i++)
+        {
+            possibleDivisions[i-1]=i;
+        }
+        this.divisionList = new JComboBox<>(this.possibleDivisions);
+        this.divisionList.setEditable(true);
+        this.divisionList.setSelectedItem(n);
+        this.divisionList.addActionListener(new eventHandling(this));
+
+        this.panel.add(divisionList, BorderLayout.NORTH);
         this.window.pack();
+    }
+    public class eventHandling implements ActionListener {
+        private plot parent;
+        public eventHandling (plot parent)
+        {
+            this.parent = parent;
+        }
 
-
+        public void actionPerformed(ActionEvent e) {
+            JComboBox cb = (JComboBox)e.getSource();
+            int newSelection = (int)cb.getSelectedItem();
+            this.parent.n = newSelection;
+            solve(this.parent.n);
+            this.parent.window.pack();
+        }
     }
 
+
     public void run(){
-        solve(9);
+        solve(n);
         window.setVisible(true);
     }
 
